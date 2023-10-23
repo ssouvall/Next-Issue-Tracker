81027d8b6773d86b005954f253ad3c5c3efa51c4
Stephen Souvall <ssouvall@agreserves.com>
2023-10-23 09:47:34 -0600

    style the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import Link from 'next/link'
-import React from 'react'
+import { usePathname } from 'next/navigation';
+import React, { useEffect } from 'react'
 import { AiFillBug } from 'react-icons/ai';
+import classnames from 'classnames';
 
 const NavBar = () => {
+  const currentPath = usePathname();
   const links = [
     { label: 'Dashboard', href:'/' },
     { label: 'Issues', href: '/issues' }
@@ -14,13 +19,16 @@ const NavBar = () => {
             {links.map(link =>             
                 <Link 
                     key={link.href} 
-                    className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                    className={classnames({
+                        'text-zinc-900': link.href === currentPath,
+                        'text-zinc-500': link.href !== currentPath,
+                        'hover:text-zinc-800 transition-colors': true
+                    })}
                     href={link.href}
                 >
                     {link.label}
                 </Link>
             )}
-            <li><Link href="/issue">Issues</Link></li>
         </ul>
     </nav>
   )

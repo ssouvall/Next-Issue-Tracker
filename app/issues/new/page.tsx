496c8b088660735a5387071b0b3a386727c2ebf6
Stephen Souvall <ssouvall@agreserves.com>
2023-10-28 07:21:41 -0600

    customize radix ui theme

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,19 @@
-'use client'
+"use client";
 
-import { Button, TextArea } from '@radix-ui/themes'
-import { TextField } from '@radix-ui/themes'
-import React from 'react'
+import { Button, TextArea } from "@radix-ui/themes";
+import { TextField } from "@radix-ui/themes";
+import React from "react";
 
 const NewIssuePage = () => {
   return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root>
-            <TextField.Input placeholder="Title" />
-        </TextField.Root>
-        <TextArea placeholder="Description" />
-        <Button>Submit New Issue</Button>
+    <div className="max-w-xl space-y-3">
+      <TextField.Root>
+        <TextField.Input placeholder="Title" />
+      </TextField.Root>
+      <TextArea placeholder="Description" />
+      <Button>Submit New Issue</Button>
     </div>
-  )
-}
+  );
+};
 
-export default NewIssuePage
+export default NewIssuePage;
